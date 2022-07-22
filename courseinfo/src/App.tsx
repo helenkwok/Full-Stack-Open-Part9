6fd7a22f9e05d546645baa71e14fe0dcb599b6b9
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-07-23 09:28:04 +0930

    9.14. Courseinfo

---
+++ courseinfo/src/App.tsx
@@ -0,0 +1,63 @@
+interface HeaderProps {
+  name: string;
+}
+
+const Header = (props: HeaderProps): JSX.Element => {
+  return <h1>{props.name}</h1>
+}
+
+interface Course {
+  name: string;
+  exerciseCount: number
+}
+
+interface CoursePartsProps {
+  parts: Course[]
+}
+
+const Content = (props: CoursePartsProps): JSX.Element => {
+  return (
+    <>{props.parts.map(part =>
+      <p key={part.name}>
+        {part.name} {part.exerciseCount}
+      </p>)
+    }</>
+  )
+}
+
+const Total = (props: CoursePartsProps): JSX.Element => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+    </p>
+  )
+}
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content parts={courseParts} />
+      <Total parts={courseParts} />
+    </div>
+  );
+};
+
+export default App;
