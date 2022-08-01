52868d2f301e7c58564105e4807d8e1c57e3bc2a
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-08-01 14:48:43 +0930

    9.20: patientor, step5

--- patientor-frontend/src/PatientPage/index.tsx
+++ patientor-frontend/src/PatientPage/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
 import axios from 'axios';
-import { Container, Typography } from '@material-ui/core';
+import { Box, Container, List, ListItem, ListItemIcon, ListItemText, Typography } from '@material-ui/core';
 import { MdMale, MdFemale, MdTransgender } from 'react-icons/md';
+import { BsDot } from "react-icons/bs";
 
 import { apiBaseUrl } from "../constants";
 import { Gender, Patient } from '../types';
@@ -18,6 +19,7 @@ const PatientPage = () => {
           const { data: patient } = await axios.get<Patient>(
           `${apiBaseUrl}/patients/${id}`);
           setPatient(patient);
+          console.log(patient.entries);
         }
       } catch (e) {
         console.error(e);
@@ -41,7 +43,7 @@ const PatientPage = () => {
     <div>
       {patient &&
         <Container>
-          <Typography style={{ marginTop: "0.5em", marginBottom: "0.5em", fontWeight: 600 }} variant="h6">
+          <Typography style={{ marginTop: "0.5em", marginBottom: "0.5em", fontWeight: 600 }} variant="h5">
             {patient.name} {getGenderIcon(patient.gender)}
           </Typography>
           <Typography variant="body1">
@@ -50,6 +52,26 @@ const PatientPage = () => {
           <Typography variant="body1">
             occupation: {patient.occupation}
           </Typography>
+          <Typography style={{ marginTop: "0.5em", marginBottom: "0.5em", fontWeight: 600 }} variant="h6">
+            entries
+          </Typography>
+            {patient.entries.map(entry =>
+              <Box key={entry.id}>
+                <Typography variant="body1">
+                  {entry.date} {entry.description}
+                </Typography>
+                <List>
+                  {entry.diagnosisCodes && entry.diagnosisCodes.map(diagnosisCode =>
+                    <ListItem key={diagnosisCode}>
+                      <ListItemIcon>
+                        <BsDot />
+                      </ListItemIcon>
+                      <ListItemText primary={diagnosisCode} />
+                    </ListItem>
+                  )}
+                </List>
+                </Box>
+            )}
         </Container>
       }
       </div>
